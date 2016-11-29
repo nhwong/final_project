581fe8a352aeaec80d03cb64540966e2a7e4d327
Nate Wong <nathaniel.h.wong@gmail.com>
2016-11-28 22:23:22 -0600

    Functioning API call

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,28 +1,39 @@
+require 'open-uri'
+
 class QueriesController < ApplicationController
   def home
   end
 
-  def retrieve
+  def results
     @dish = Dish.where(:dish => params[:query])
-    # @dish = Dish.first
     @query = Query.new
 
     if @dish.count == 0
       @dish = Dish.new
       @dish.dish = params[:query]
-      # dish_save_status = @dish.save
-      # if dish_save_status != true
-      #   render("/queries", :notice => "Unsuccessful query")
-      # end
-      #
-      # @query.dish_id = @dish.id
-      # if query_save_status != true
-      # query_save_status = @query.save
-      #   render("/queries", :notice => "Unsuccessful query")
-      # end
+      dish_save_status = @dish.save
+      if dish_save_status != true
+        redirect_to("", :notice => "Unsuccessful query")
+      end
+
+      @query.dish_id = @dish.id
+      query_save_status = @query.save
+      if query_save_status != true
+        redirect_to("", :notice => "Unsuccessful query")
+      end
     end
 
-    render("home")
+    food_2_fork_search_url = "http://food2fork.com/api/search?key=9d4bb2fbc1fb896ab72ed80caa894eb2&q="
+    full_url = food_2_fork_search_url + params[:query]
+    food_2_fork_hash = JSON.parse(open(full_url).read)
+    @result_hash = food_2_fork_hash
+    @recipe = get_recipes(@result_hash)
+  end
+
+  def get_recipes(hash)
+    rId = hash["recipes"][0]["recipe_id"]
+    food_2_fork_recipe_url = "http://food2fork.com/api/get?key=9d4bb2fbc1fb896ab72ed80caa894eb2&rId=" + rId.to_s
+    return JSON.parse(open(food_2_fork_recipe_url).read)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "queries#home"
 
-  get "queries/retrieve"
+  get "queries/results"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
